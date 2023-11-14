6086f6cb93ffff0781e237698772a84742fa2f6a
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-11-15 01:46:40 +0200

    edit: image quality

--- src/components/FormImagePicker.tsx
+++ src/components/FormImagePicker.tsx
@@ -25,7 +25,7 @@ const FormImagePicker = ({contentContainerStyle, control, name}: Props) => {
   const handlePickImage = async () => {
     const {assets} = await launchImageLibrary({
       mediaType: 'photo',
-      quality: 0.1,
+      quality: 0.3,
     });
 
     if (assets) {
