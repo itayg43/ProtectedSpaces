6651e7a046107a5b90454973a26c514eb2f6a615
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-11-25 04:05:44 +0200

    edit: move protected spaces map into protected spaces screen

--- src/screens/ProtectedSpacesScreen.tsx
+++ src/screens/ProtectedSpacesScreen.tsx
@@ -1,29 +1,69 @@
 import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
 import {FAB} from 'react-native-paper';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
-import ProtectedSpacesMap from '../components/ProtectedSpacesMap';
 import KeyboardAvoidingView from '../components/KeyboardAvoidingView';
 import {useLocationContext} from '../contexts/locationContext';
 import Modal from '../components/Modal';
 import AddProtectedSpaceForm from '../components/forms/AddProtectedSpaceForm';
 
+import {ProtectedSpacesScreenNavigationProp} from '../navigators/ProtectedSpacesStack';
+import {useProtectedSpacesContext} from '../contexts/protectedSpacesContext';
+import {DEFAULT_MAP_DELTAS, DEFAULT_MAP_REGION} from '../utils/constants';
+
 const ProtectedSpacesScreen = () => {
   const safeAreaInsets = useSafeAreaInsets();
 
+  const navigation = useNavigation<ProtectedSpacesScreenNavigationProp>();
+
   const location = useLocationContext();
+
+  const {protectedSpaces} = useProtectedSpacesContext();
 
   const [showAddModal, setShowAddModal] = useState(false);
 
+  const handleMarkerPress = (id: string) => {
+    navigation.navigate('protectedSpaceDetailsScreen', {
+      id,
+    });
+  };
+
   const handleToggleShowAddModal = () => {
-    setShowAddModal(currState => !currState);
+    setShowAddModal(currentState => !currentState);
   };
 
   return (
     <KeyboardAvoidingView>
       <View style={styles.container}>
-        <ProtectedSpacesMap />
+        <MapView
+          style={styles.mapContainer}
+          provider={PROVIDER_GOOGLE}
+          region={
+            location
+              ? {
+                  latitude: location.latitude,
+                  longitude: location.longitude,
+                  latitudeDelta: DEFAULT_MAP_DELTAS.LATITUDE,
+                  longitudeDelta: DEFAULT_MAP_DELTAS.LONGITUDE,
+                }
+              : DEFAULT_MAP_REGION
+          }
+          showsUserLocation>
+          {location &&
+            protectedSpaces.map(s => (
+              <Marker
+                key={s.id}
+                coordinate={{
+                  latitude: s.address.latLng.latitude,
+                  longitude: s.address.latLng.longitude,
+                }}
+                onPress={() => handleMarkerPress(s.id)}
+              />
+            ))}
+        </MapView>
 
         {location && (
           <FAB
@@ -33,6 +73,8 @@ const ProtectedSpacesScreen = () => {
             onPress={handleToggleShowAddModal}
           />
         )}
+
+        {/** MODALS */}
 
         {showAddModal && (
           <Modal isVisible={showAddModal} onDismiss={handleToggleShowAddModal}>
@@ -51,6 +93,10 @@ export default ProtectedSpacesScreen;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+
+  mapContainer: {
     flex: 1,
   },
 
