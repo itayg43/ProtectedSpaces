5fda60a73426658a4e72e44622c849c9d0a6fb2c
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-11-25 03:52:48 +0200

    edit: delete loading view

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import LoginScreen from './screens/LoginScreen';
 import {useAuthContext} from './contexts/authContext';
 import {ProtectedSpacesContextProvider} from './contexts/protectedSpacesContext';
 import {LocationContextProvider} from './contexts/locationContext';
-import LoadingView from './components/LoadingView';
 import ProtectedSpacesStackNavigator from './navigators/ProtectedSpacesStack';
 
 const App = () => {
   const {isInitializing, user} = useAuthContext();
 
   if (isInitializing) {
-    return <LoadingView />;
+    return null;
   }
 
   return user ? (
