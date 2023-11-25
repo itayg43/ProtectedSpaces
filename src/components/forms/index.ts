9af11d7a7bd647568a88e0ea010ec78459e265d2
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-11-25 03:53:35 +0200

    edit: delete form index file

--- src/components/forms/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as FormTextInput} from './FormTextInput';
-export {default as FormGooglePlacesAutocomplete} from './FormGooglePlacesAutocomplete';
-export {default as FormSegmentedButtons} from './FormSegmentedButtons';
-export {default as FormImagesPicker} from './FormImagesPicker';
