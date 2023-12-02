83da9650deba932762e74cbbda5fb474e5915707
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-12-02 03:32:11 +0200

    edit: delete unused constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,10 +2,3 @@ export const DEFAULT_MAP_DELTAS = {
   LATITUDE: 0.01,
   LONGITUDE: 0.01,
 };
-
-export const DEFAULT_MAP_REGION = {
-  latitude: 31.5,
-  longitude: 34.75,
-  latitudeDelta: 3,
-  longitudeDelta: 0.01,
-};
