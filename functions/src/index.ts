7a04ff5a1c37a7781d79d033f9dea22c7f91f6e4
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-12-06 03:01:25 +0200

    edit: create delete images in firestore functions

---
+++ functions/src/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable quotes */
+
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+export const deleteImages = functions.firestore
+  .document('ProtectedSpaces/{id}')
+  .onDelete(async (_, context) => {
+    try {
+      const {id} = context.params;
+
+      console.log(`Delete images of protected space with the id: ${id}`);
+
+      await admin
+        .storage()
+        .bucket()
+        .deleteFiles({
+          prefix: `images/${id}`,
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  });
