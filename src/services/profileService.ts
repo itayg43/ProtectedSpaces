577697b491f521d70ac8bd9dcc0fda30d50e2a11
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-01-02 00:17:11 +0200

    edit: add, set, and remove spaces

--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -1,13 +1,22 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import log from '../utils/log';
+import {Space} from '../utils/types';
 
 const RADIUS_KEY = 'radiusInM';
 export const DEFAULT_RADIUS_IN_M = 50;
 
+const SPACES_KEY = 'spaces';
+const DEFAULT_SPACES: Space[] = [];
+
 const setRadius = async (value: number) => {
   await AsyncStorage.setItem(RADIUS_KEY, value.toString());
   log.debug(`SET ${RADIUS_KEY}: ${value}`);
+};
+
+const setSpaces = async (values: Space[]) => {
+  await AsyncStorage.setItem(SPACES_KEY, JSON.stringify(values));
+  log.debug(`SET ${SPACES_KEY}: ${JSON.stringify(values, null, 2)}`);
 };
 
 const getRadius = async () => {
@@ -23,13 +32,34 @@ const getRadius = async () => {
   return parseInt(storedValue, 10);
 };
 
+const getSpaces = async () => {
+  const storedValue = await AsyncStorage.getItem(SPACES_KEY);
+
+  if (storedValue === null) {
+    log.debug(`GET ${SPACES_KEY}: null`);
+    setSpaces(DEFAULT_SPACES);
+    return DEFAULT_SPACES;
+  }
+
+  log.debug(`GET ${SPACES_KEY}: ${storedValue}`);
+  return JSON.parse(storedValue) as Space[];
+};
+
 const removeRadius = async () => {
   await AsyncStorage.removeItem(RADIUS_KEY);
   log.debug(`REMOVE ${RADIUS_KEY}`);
 };
 
+const removeSpaces = async () => {
+  await AsyncStorage.removeItem(SPACES_KEY);
+  log.debug(`REMOVE ${SPACES_KEY}`);
+};
+
 export default {
   setRadius,
+  setSpaces,
   getRadius,
+  getSpaces,
   removeRadius,
+  removeSpaces,
 };
