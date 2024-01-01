95d4e1933720e09d747c558d081cb944e6d15252
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-01-01 05:47:34 +0200

    refactor: create reducer

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -4,62 +4,90 @@ import React, {
   useCallback,
   useContext,
   useEffect,
-  useState,
 } from 'react';
 import {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {statusCodes} from '@react-native-google-signin/google-signin';
+import {useImmerReducer} from 'use-immer';
 
 import authService from '../services/authService';
 import type {AuthProvider, RequestStatus} from '../utils/types';
 import alert from '../utils/alert';
 import log from '../utils/log';
-import {statusCodes} from '@react-native-google-signin/google-signin';
 
-type AuthContextParams = {
+enum AuthReducerActionType {
+  SIGN_IN = 'SIGN_IN',
+  SIGN_IN_CANCELLED = 'SIGN_IN_CANCELLED',
+  SIGN_IN_FAIL = 'SIGN_IN_FAIL',
+  AUTH_STATE_CHANGE = 'AUTH_STATE_CHANGE',
+}
+
+type AuthReducerData = {
   status: RequestStatus;
-  isNewSignIn: boolean;
   user: FirebaseAuthTypes.User | null;
+  lastAction: AuthReducerActionType | null;
+};
+const initialReducerData: AuthReducerData = {
+  status: 'idle',
+  user: null,
+  lastAction: null,
+};
+
+type AuthContextParams = AuthReducerData & {
   handleSignIn: (provider: AuthProvider) => Promise<void>;
   handleSignOut: () => Promise<void>;
 };
 
-const initialContextParams: AuthContextParams = {
-  status: 'idle',
-  isNewSignIn: false,
-  user: null,
+const AuthContext = createContext<AuthContextParams>({
+  ...initialReducerData,
   handleSignIn: async () => {},
   handleSignOut: async () => {},
-};
+});
 
-const AuthContext = createContext<AuthContextParams>(initialContextParams);
+type AuthReducerAction =
+  | {
+      type: AuthReducerActionType.SIGN_IN;
+    }
+  | {
+      type: AuthReducerActionType.SIGN_IN_CANCELLED;
+    }
+  | {
+      type: AuthReducerActionType.SIGN_IN_FAIL;
+    }
+  | {
+      type: AuthReducerActionType.AUTH_STATE_CHANGE;
+      payload: {
+        user: FirebaseAuthTypes.User | null;
+      };
+    };
 
 export const AuthContextProvider = ({children}: PropsWithChildren) => {
-  const [status, setStatus] = useState<RequestStatus>('idle');
-  const [isNewSignIn, setIsNewSignIn] = useState(false);
-  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(
-    authService.getCurrentUser(),
+  const [data, dispatch] = useImmerReducer<AuthReducerData, AuthReducerAction>(
+    authReducer,
+    initialReducerData,
   );
 
-  const handleSignIn = useCallback(async (provider: AuthProvider) => {
-    try {
-      setStatus('loading');
-      await authService.signIn(provider);
-      setIsNewSignIn(true);
-    } catch (error: any) {
-      if (error?.code === statusCodes.SIGN_IN_CANCELLED) {
-        setStatus('idle');
-        return;
-      }
+  const handleSignIn = useCallback(
+    async (provider: AuthProvider) => {
+      try {
+        dispatch({type: AuthReducerActionType.SIGN_IN});
+        await authService.signIn(provider);
+      } catch (error: any) {
+        if (error?.code === statusCodes.SIGN_IN_CANCELLED) {
+          dispatch({type: AuthReducerActionType.SIGN_IN_CANCELLED});
+          return;
+        }
 
-      log.error(error);
-      setStatus('error');
-      alert.error('Sign in error');
-    }
-  }, []);
+        log.error(error);
+        dispatch({type: AuthReducerActionType.SIGN_IN_FAIL});
+        alert.error('Sign in error');
+      }
+    },
+    [dispatch],
+  );
 
   const handleSignOut = useCallback(async () => {
     try {
       await authService.signOut();
-      setIsNewSignIn(false);
     } catch (error) {
       log.error(error);
       throw new Error('Sign out error');
@@ -67,11 +95,15 @@ export const AuthContextProvider = ({children}: PropsWithChildren) => {
   }, []);
 
   const handleAuthStateChange = useCallback(
-    (u: FirebaseAuthTypes.User | null) => {
-      setUser(u);
-      setStatus('idle');
+    (user: FirebaseAuthTypes.User | null) => {
+      dispatch({
+        type: AuthReducerActionType.AUTH_STATE_CHANGE,
+        payload: {
+          user,
+        },
+      });
     },
-    [],
+    [dispatch],
   );
 
   useEffect(() => {
@@ -83,9 +115,9 @@ export const AuthContextProvider = ({children}: PropsWithChildren) => {
   return (
     <AuthContext.Provider
       value={{
-        status,
-        isNewSignIn,
-        user,
+        status: data.status,
+        user: data.user,
+        lastAction: data.lastAction,
         handleSignIn,
         handleSignOut,
       }}>
@@ -95,3 +127,32 @@ export const AuthContextProvider = ({children}: PropsWithChildren) => {
 };
 
 export const useAuthContext = () => useContext(AuthContext);
+
+function authReducer(draft: AuthReducerData, action: AuthReducerAction) {
+  switch (action.type) {
+    case AuthReducerActionType.SIGN_IN: {
+      draft.status = 'loading';
+      draft.lastAction = action.type;
+      break;
+    }
+
+    case AuthReducerActionType.SIGN_IN_CANCELLED: {
+      draft.status = 'idle';
+      draft.lastAction = action.type;
+      break;
+    }
+
+    case AuthReducerActionType.SIGN_IN_FAIL: {
+      draft.status = 'error';
+      draft.lastAction = action.type;
+      break;
+    }
+
+    case AuthReducerActionType.AUTH_STATE_CHANGE: {
+      const {user} = action.payload;
+      draft.status = 'idle';
+      draft.user = user;
+      break;
+    }
+  }
+}
